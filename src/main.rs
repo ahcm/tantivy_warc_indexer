a59e29f3118bb456dd7725195049fe55f3420c7c
Andreas Hauser <andy@creative-memory.de>
2021-05-06 12:06:04 +0000

    Clean up.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,15 @@
 use std;
 use std::io;
 use std::fs::File;
-use std::os::unix::io::FromRawFd;
 use std::io::BufRead;
 use std::io::prelude::*;
 use std::collections::HashMap;
 use std::path::PathBuf;
 
 use docopt::Docopt;
-use serde::Deserialize;
 extern crate tantivy;
 use tantivy::Document;
 use tantivy::Index;
-use tantivy::IndexWriter;
-use tantivy::ReloadPolicy;
-use tantivy::collector::TopDocs;
-use tantivy::merge_policy::NoMergePolicy;
-use tantivy::query::QueryParser;
-use tantivy::schema::*;
 
 const USAGE: &'static str = "
 WARC Indexer
@@ -26,12 +18,11 @@ Usage:
   warc_parser  <index> <warc_dir>
 ";
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug)]
 struct Args {
     arg_index: Vec<String>,
     arg_warc_dir: Vec<String>,
 }
-
 
 #[derive(Debug)]
 enum WARCType
@@ -74,7 +65,7 @@ fn read_record(reader: & mut io::BufReader<File>) ->  Result<Option<WARCRecord>,
             [ "WARC/1.0" ] => header.insert("WARC".to_string(),"1.0".to_string()),
             [ "" ]         => // end of header     
                 {
-                    let warc_version   = header.get("WARC").expect("Header Field WARC version missing.");
+                    let warc_version   = header.get("WARC").expect("Header Field WARC version missing.").clone();
                     let warc_type      = match header.get("WARC-Type").expect("Header Field WARC-Type missing").as_ref()
                     {
                           "conversion" => WARCType::Conversion,
@@ -85,7 +76,7 @@ fn read_record(reader: & mut io::BufReader<File>) ->  Result<Option<WARCRecord>,
                             "request"  => WARCType::Request,
                             "resource" => WARCType::Resource,
                             "response" => WARCType::Response,
-                                 other => panic!(String::from("Not a known WARCType: ").push_str(other)),
+                                 other => panic!("Not a known WARCType: {}", other),
                     };
                     let content_length = header
                                          .get("Content-Length").expect("Header Field Content-Length missing")
@@ -104,11 +95,11 @@ fn read_record(reader: & mut io::BufReader<File>) ->  Result<Option<WARCRecord>,
                     reader.lines().next();
                     reader.lines().next();
 
-                    return Ok(Some(WARCRecord{warc_version : warc_version.clone(),
-                                                 warc_type : warc_type,
-                                            content_length : content_length,
-                                                   payload : payload,
-                                                    header : header}));
+                    return Ok(Some(WARCRecord{warc_version,
+                                              warc_type,
+                                              content_length,
+                                              payload,
+                                              header}));
                 }
             [ _, _, .. ]   => {eprintln!("ignoring unexpected multiple values");  break}, 
             [ _ ]          => {eprintln!("ignoring unexpected single value");  break},
@@ -123,10 +114,8 @@ fn read_record(reader: & mut io::BufReader<File>) ->  Result<Option<WARCRecord>,
 fn main() -> Result<(), std::io::Error>
 {
     let args = Docopt::new(USAGE)
-               .and_then(|d| d.argv(std::env::args().into_iter()).parse())
-                  .unwrap_or_else(|e| e.exit());
-//        .and_then(|d| d.deserialize())
-//        .unwrap_or_else(|e| e.exit());
+        .and_then(|d| d.argv(std::env::args().into_iter()).parse())
+        .unwrap_or_else(|e| e.exit());
 
     let index_dir = args.get_str("<index>");
     let warc_dir = args.get_str("<warc_dir>");
@@ -134,13 +123,21 @@ fn main() -> Result<(), std::io::Error>
     println!("{:?}", warc_dir);
     let index_directory = PathBuf::from(index_dir);
     let index = Index::open_in_dir(&index_directory).expect("Tantivy Index Directory open failed");
+    let mut index_writer = index.writer_with_num_threads(64, 64 * 4095 * 1024 * 1024).expect("index writer failed");
+
     let schema = index.schema();
-    let mut index_writer = index.writer(32293967295).expect("index writer failed");
-    //let stdin = io::stdin();
-    //let handle = stdin.lock();
+    let schema_uri   = schema.get_field("uri").unwrap();
+    let schema_title = schema.get_field("title").unwrap();
+    let schema_body  = schema.get_field("body").unwrap();
+    let schema_date  = schema.get_field("date").unwrap();
+
+    let mut numfiles = 0;
     for path in std::fs::read_dir(warc_dir).unwrap()
     {
-        let file = File::open(path.unwrap().path()).unwrap();
+        numfiles += 1;
+        let filename = path.unwrap().path();
+        eprintln!("{}\t{}", numfiles, filename.to_string_lossy());
+        let file = File::open(filename).unwrap();
         let mut reader = io::BufReader::with_capacity(600 * 1024 * 1024, file);
         let mut count = 0;
         while let Some(record) = read_record(& mut reader)?
@@ -149,35 +146,20 @@ fn main() -> Result<(), std::io::Error>
             {
                 WARCType::WarcInfo =>
                 {
-                    eprintln!("{}", String::from_utf8(record.payload).expect("warcinfo in UTF-8"));
+                    //eprintln!("{}", String::from_utf8(record.payload).expect("warcinfo in UTF-8"));
                 },
                 WARCType::Conversion =>
                 {
-                    //eprintln!("{:?}", &record.warc_type);
-                    //eprintln!("{:?}", &record.header);
-                    //eprintln!("{:?}", &record.header.get("WARC-Target-URI"));
-                    let body = std::str::from_utf8(&record.payload).expect("convert to utf8 failed");
                     count += 1;
-                    if count % 1000 == 0
-                    {
-                        eprint!(".");
-                        //println!("{}", record.content_length);
-                    }
-                    //let doc = schema.parse_document("");
-                    //index_writer.add_document(doc);
-                    let schema_uri   = schema.get_field("uri").unwrap();
-                    let schema_title = schema.get_field("title").unwrap();
-                    let schema_body  = schema.get_field("body").unwrap();
-                    let schema_date  = schema.get_field("date").unwrap();
+                    if count % 1000 == 0 { eprint!("."); }
 
+                    let body = std::str::from_utf8(&record.payload).expect("convert to utf8 failed");
                     let mut doc = Document::default();
                     doc.add_text(schema_uri, &record.header.get("WARC-Target-URI").expect("get uri"));
                     doc.add_text(schema_date, &record.header.get("WARC-Date").expect("get date"));
                     doc.add_text(schema_body, body);
                     doc.add_text(schema_title, body.lines().next().expect("title"));
                     index_writer.add_document(doc);
-
-
                 }
                 _ => ()
             }
