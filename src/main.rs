09c689e5982a2ac60bd776dce7e74635251bfb2a
Andreas Hauser <andy@creative-memory.de>
2021-08-01 21:49:12 +0000

    Detect and read gzipped wet files

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
+#![feature(associated_type_bounds)]
 use std;
 use std::io;
 use std::fs::File;
 use std::io::BufRead;
-use std::io::prelude::*;
 use std::collections::HashMap;
 use std::path::PathBuf;
+use std::ffi::OsStr;
 
 use docopt::Docopt;
 extern crate tantivy;
 use tantivy::Document;
 use tantivy::Index;
+use tantivy::IndexWriter;
+use std::io::Read;
+use flate2::read::MultiGzDecoder;
+
 
 const USAGE: &'static str = "
 WARC Indexer
 
 Usage:
-  warc_parser [-t <threads>]  <index> <warc_dir>
+  warc_parser [-t <threads>] [--from <from>] [--to <to>] <index> <warc_dir>
   warc_parser (-h | --help)
 
 Options:
-  -h --help     Show this help
-  -t <threads>  number of threads to use, default 4
+  -h --help      Show this help
+  -t <threads>   number of threads to use, default 4
+  --from <from>  skip files until from
+  --to <to>      skip files after to
 ";
 
 #[derive(Debug)]
@@ -58,7 +65,7 @@ struct WARCRecord
 
 }
 
-fn read_record(reader: & mut io::BufReader<File>) ->  Result<Option<WARCRecord>, std::io::Error>
+fn read_record(reader: &mut dyn io::BufRead) ->  Result<Option<WARCRecord>, std::io::Error>
 {
     let mut header: HashMap<String, String> = HashMap::new();
     for line in reader.lines()
@@ -117,6 +124,45 @@ fn read_record(reader: & mut io::BufReader<File>) ->  Result<Option<WARCRecord>,
 
 }
 
+
+fn extract_records_and_add_to_index(index: &Index, index_writer : &IndexWriter, reader : &mut dyn BufRead) -> io::Result<()>
+{
+    let schema = index.schema();
+    let schema_uri   = schema.get_field("uri").unwrap();
+    let schema_title = schema.get_field("title").unwrap();
+    let schema_body  = schema.get_field("body").unwrap();
+    let schema_date  = schema.get_field("date").unwrap();
+
+    let mut count = 0;
+    while let Some(record) = read_record(reader)?
+    {
+        match record.warc_type
+        {
+            WARCType::WarcInfo =>
+            {
+                //eprintln!("{}", String::from_utf8(record.payload).expect("warcinfo in UTF-8"));
+            },
+            WARCType::Conversion =>
+            {
+                count += 1;
+                if count % 1000 == 0 { eprint!("."); }
+
+                let body = std::str::from_utf8(&record.payload).expect("convert to utf8 failed");
+                let mut doc = Document::default();
+                doc.add_text(schema_uri, &record.header.get("WARC-Target-URI").expect("get uri"));
+                doc.add_text(schema_date, &record.header.get("WARC-Date").expect("get date"));
+                doc.add_text(schema_body, body);
+                doc.add_text(schema_title, body.lines().next().expect("title"));
+                index_writer.add_document(doc);
+            }
+            _ => ()
+        }
+    }
+
+    println!("\nTotal Records of WARC file processed: {}", count);
+    Ok(())
+}
+
 fn main() -> Result<(), std::io::Error>
 {
     let args = Docopt::new(USAGE)
@@ -124,58 +170,61 @@ fn main() -> Result<(), std::io::Error>
         .unwrap_or_else(|e| e.exit());
 
     let index_dir = args.get_str("<index>");
-    let warc_dir = args.get_str("<warc_dir>");
-    let threads = args.get_str("-t");
+    let warc_dir  = args.get_str("<warc_dir>");
+    let threads   = args.get_str("-t");
+    let from      = args.get_str("--from").parse::<usize>().unwrap_or(0);
+    let to        = args.get_str("--to").parse::<usize>().unwrap_or(usize::MAX);
     let nthreads : usize = threads.parse().unwrap_or(4);
+    const PER_THREAD_BUF_SIZE : usize = 600 * 1024 * 1024;
+
+    println!("Only indexing files: {} - {}", from, to);
     println!("Index dir: {:?}", index_dir);
     println!("Warc dir: {:?}", warc_dir);
     println!("Threads: {:?}", nthreads);
+    println!("");
+
     let index_directory = PathBuf::from(index_dir);
     let index = Index::open_in_dir(&index_directory).expect("Tantivy Index Directory open failed");
     let mut index_writer = index.writer_with_num_threads(nthreads, nthreads * 4095 * 1024 * 1024).expect("index writer failed");
-
-    let schema = index.schema();
-    let schema_uri   = schema.get_field("uri").unwrap();
-    let schema_title = schema.get_field("title").unwrap();
-    let schema_body  = schema.get_field("body").unwrap();
-    let schema_date  = schema.get_field("date").unwrap();
 
     let mut numfiles = 0;
     for path in std::fs::read_dir(warc_dir).unwrap()
     {
         numfiles += 1;
-        let filename = path.unwrap().path();
-        eprintln!("{}\t{}", numfiles, filename.to_string_lossy());
-        let file = File::open(filename).unwrap();
-        let mut reader = io::BufReader::with_capacity(600 * 1024 * 1024, file);
-        let mut count = 0;
-        while let Some(record) = read_record(& mut reader)?
+        if numfiles < from || numfiles > to
         {
-            match record.warc_type
-            {
-                WARCType::WarcInfo =>
-                {
-                    //eprintln!("{}", String::from_utf8(record.payload).expect("warcinfo in UTF-8"));
-                },
-                WARCType::Conversion =>
-                {
-                    count += 1;
-                    if count % 1000 == 0 { eprint!("."); }
-
-                    let body = std::str::from_utf8(&record.payload).expect("convert to utf8 failed");
-                    let mut doc = Document::default();
-                    doc.add_text(schema_uri, &record.header.get("WARC-Target-URI").expect("get uri"));
-                    doc.add_text(schema_date, &record.header.get("WARC-Date").expect("get date"));
-                    doc.add_text(schema_body, body);
-                    doc.add_text(schema_title, body.lines().next().expect("title"));
-                    index_writer.add_document(doc);
-                }
-                _ => ()
-            }
+            continue
         }
-        println!("\nTotal records: {}", count);
-    }
+        let filename = path.unwrap().path();
+        let file = File::open(&filename).unwrap();
+        
+        eprintln!("{}\t{}", numfiles, filename.to_string_lossy());
+        match filename.extension()
+        {
+            Some(extension) =>
+                if extension == OsStr::new("gz")
+                {
+                    extract_records_and_add_to_index(&index,
+                                                     &index_writer,
+                                                     &mut io::BufReader::with_capacity(PER_THREAD_BUF_SIZE, MultiGzDecoder::new(file) )
+                                                    )?;
+                }
+                else if extension == OsStr::new("wet")
+                {
+                    extract_records_and_add_to_index(&index,
+                                                     &index_writer,
+                                                     &mut io::BufReader::with_capacity(PER_THREAD_BUF_SIZE, file)
+                                                    )?;
+                }
+                else
+                {
+                    eprintln!("Skip file, neither wet nor gz");
+                },
+            None => 
+                    eprintln!("Skip file, neither wet nor gz"),
 
+        }
+    }
     index_writer.commit().expect("commit");
     index_writer.wait_merging_threads().expect("merging");
 
