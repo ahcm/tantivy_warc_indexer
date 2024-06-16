bc878e8bb5154a2652ff787e12789be2b118a6f2
Andreas Hauser <andreas.cm@icloud.com>
2024-06-17 01:50:38 +0200

    io_read_to_string is stable now

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(io_read_to_string)]
+//#![feature(io_read_to_string)]
 pub mod warc;
 pub mod pubmed;
 pub mod wikipedia_abstract;
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![feature(associated_type_bounds)]
+//#![feature(associated_type_bounds)]
 use std;
 use std::io;
 use std::fs::File;
